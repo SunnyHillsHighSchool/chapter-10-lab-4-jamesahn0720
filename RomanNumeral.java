d023eb9bcdb8883d91965de2aad7dbd700d5dcc8
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 21:58:35 -0700

    Update RomanNumeral.java

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -9,11 +9,9 @@ public class RomanNumeral
 	private Integer number;
 	private String roman;
 
-	private final static int[] NUMBERS= {1000,900,500,400,100,90,
-													50,40,10,9,5,4,1};
+	private final static int[] NUMBERS= {1000,900,500,400,100,90,50,40,10,9,5,4,1};
 
-	private final static String[] LETTERS = {"M","CM","D","CD","C","XC",
-												  "L","XL","X","IX","V","IV","I"};
+	private final static String[] LETTERS = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
 	public RomanNumeral(String str)
 	{
